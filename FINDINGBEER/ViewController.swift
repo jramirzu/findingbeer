eeacdb1f0ec080566ff9b80e6f0f384919fc8391
Jorge Jair Ramirez Gaston Zuloeta <jramirzu@emeal.nttdata.com>
2022-06-21 19:01:53 -0500

    First commit

---
+++ FINDINGBEER/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FINDINGBEER
+//
+//  Created by Jorge Jair Ramirez Gaston Zuloeta on 21/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+Hola mundo
+
