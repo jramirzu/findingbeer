b0b35c05d852527cec054b4da435ac1984b86ac3
Jorge Jair Ramirez Gaston Zuloeta <jramirzu@emeal.nttdata.com>
2022-07-10 01:36:00 -0500

    Correciones Lista JSON

---
+++ FINDINGBEER/CustomTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTableViewCell.swift
+//  FINDINGBEER
+//
+//  Created by Jorge Jair Ramirez Gaston Zuloeta on 10/07/22.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
